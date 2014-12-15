70106ccecb54ce19a8ec0b5dab64e7228e93606b
miguel angel fernandez sanchez <maferna@zeta30.aulas.gsyc.es>
2014-12-15 16:54:27 +0100

    Implementado fichero de log

--- uaclient.py
+++ uaclient.py
@@ -8,6 +8,7 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 import os
+import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
@@ -74,6 +75,24 @@ class SIPConfigLocal:
     def get_tags(self):
         return self.dicc_datos
 
+def write_log(fichero, evento):
+    datime = time.strftime("%Y%m%d%Y%H%M%S", time.gmtime())
+    linea = str(datime) + ' ' + evento + '\r\n'
+    fichero.write(linea)
+
+def formar_evento(tipo, datos, ip, puerto):
+
+    accion = tipo
+    if tipo == 'envio':
+        accion = "Sent to " + ip + ':' + puerto + " "
+    elif tipo == 'recepcion':
+        accion = "Received from " + ip + ':' + puerto + " "
+    elif tipo == 'error':
+        accion = 'Error: '
+
+    frase = accion + datos
+    return frase
+
 if __name__ == "__main__":
     """
     Programa principal 
@@ -94,15 +113,20 @@ if __name__ == "__main__":
             mi_user = SIPConfigLocal(fichero)
 
     method_list = ['REGISTER', 'INVITE', 'BYE']
-
-
-
+    lista_ack = ['SIP/2.0', '100', 'Trying', 'SIP/2.0', '180',
+             'Ringing', 'SIP/2.0', '200', 'OK']
     VER = "SIP/2.0"
     datos_sesion = mi_user.get_tags()
+    log_path = str(datos_sesion['log_path'])
+    log_fich = open(log_path, 'w')
+    log_fich.close()
+    log_fich = open(log_path, 'a')
+    evento = formar_evento('Starting','...','','')
+    write_log(log_fich, evento)
+
     METODO = arg_term[2]
     OPCION = arg_term[3]
     RECEPTOR = datos_sesion['account_username']
-    print RECEPTOR
     
     # Comprobamos si el método es conocido
     if METODO not in method_list:
@@ -112,9 +136,9 @@ if __name__ == "__main__":
     # Dirección IP del servidor.
     IP = datos_sesion['uaserver_ip']
     # Comprobamos si el puerto introducido es correcto
-    PORT = datos_sesion['uaserver_port']
+
     try:
-        PORT = int(user_info[1].split(":")[1])
+        PORT = int(datos_sesion['uaserver_puerto'])
     except ValueError:
         print usage
         raise SystemExit
@@ -131,11 +155,20 @@ if __name__ == "__main__":
     # Comprobamos si hay un servidor escuchando
     try:
         print "Enviando: " + LINE
+        evento = formar_evento('envio', LINE, IP, str(PORT))
+        write_log(log_fich, evento)
         my_socket.send(LINE + '\r\n')
         data = my_socket.recv(1024)
+        evento = formar_evento('recepcion', data, '', '')
+        write_log(log_fich, evento)
         print "Recibido: " + data
     except socket.error:
-        print "Error: No server listening at " + IP + " port " + str(PORT)
+        descrip = "No server listening at " + IP + " port " + str(PORT)
+        evento = formar_evento('error', descrip,'','')
+        write_log(log_fich, evento)
+        print "Error: " + descrip
+        evento = formar_evento('Finishing', '...','','')
+        write_log(log_fich, evento)
         raise SystemExit
 
     if data.split() == lista_ack:
