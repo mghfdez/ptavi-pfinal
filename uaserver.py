ac2c2c78f81dc131c35d70240d41f3cd85e50f6c
igarag <naxo.elderane@gmail.com>
2015-01-04 15:34:43 +0100

    Primera aproximación

--- uaserver.py
+++ uaserver.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-IGNACIO ARRANZ ÁGUEDA - ISAM - PTAVI - PRACTICA FINAL
+IGNACIO ARRANZ ÁGUEDA - ISAM - PTAVI - PRACTICA FINAL - UASERVER
 """
 
 import SocketServer
 import sys
 import os
+import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
 metodos = ("INVITE", "BYE", "ACK")
+port_rtp_dic = {}
 
-def log_status (Estado):
-    fich = open("LOG_SERVER", "a")
+
+def log_status(Estado):
+    fich = open("LOG_SERVER.txt", "a")
     fich.write(time.strftime('%Y%m%d%H%M%S '))
     fich.write(Estado+"\r")
 
+
 # ================================ OBJETOS =================================
 class XMLHandler(ContentHandler):
+    """
+    Extrae los campos del fichero .xml
+    """
     def __init__(self):
         # Declaramos el diccionario
         self.elementos = {}
         self.tags = ["account", "uaserver", "rtpaudio", "regproxy", "log", "audio"]
         self.atributos = {
             "account": ["username", "passwd"],
-            "uaserver": ["ip","puerto"],
+            "uaserver": ["ip", "puerto"],
             "rtpaudio": ["puerto"],
             "regproxy": ["ip", "puerto"],
             "log": ["path"],
@@ -35,12 +42,11 @@ class XMLHandler(ContentHandler):
 
     def get_tags(self):
         return self.elementos
-    
+
     def startElement(self, name, attrs):
         if name in self.tags:
             for atributo in self.atributos[name]:
-                self.elementos[name + "_" + atributo] = attrs.get(atributo, "") 
-
+                self.elementos[name + "_" + atributo] = attrs.get(atributo, "")
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -59,8 +65,12 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             else:
                 if "\r\n\r\n" in line:
-                    print "Mensaje de entrada " + line
+                    print
+                    print "MENSAJE DE ENTRADA"
+                    print "=================="
+                    print line
                     line = line.split()
+                    print line
 
                     if ("sip:" in line[1][:4]) and \
                             ("@" in line[1]) and line[2] == 'SIP/2.0':
@@ -72,24 +82,67 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                             self.wfile.write("SIP/2.0 405 Method \
                                 Not Allowed\r\n\r\n")
                         else:
+# =============================== INVITE ==================================
+
                             if METODO == "INVITE" and line[2] == "SIP/2.0":
+                                Inv_log = "Received INVITE from " \
+                                    + str(line[1]) + ":" + "200 OK [...]"
+                                log_status(Inv_log)
+
+                                # Guardamos en un diccionario el usuario y
+                                # puerto que entra
+                                port_rtp_dic[line[1][1]] = line[-2]
+
+                                # Se forma el SPD
+                                NAME = line[6].split("=")[1]
+                                RTP_PORT = myHandler.elementos["rtpaudio_puerto"]
+
+                                SDP = "v=0\r\n" + "o=" + NAME + "\r\n" \
+                                    + "s=SesionSIP\r\n" + "m=audio " \
+                                    + str(RTP_PORT) + " RTP"
+
+                                LINE = METODO + " sip:" + NAME + " SIP/2.0\r\n"
+                                LINE += "Content-Type: application/sdp\r\n\r\n"
+                                LINE += str(SDP)
 
                                 Answer = "SIP/2.0 100 Trying\r\n\r\n" + \
                                     "SIP/2.0 180 Ringing\r\n\r\n" + \
-                                    "SIP/2.0 200 OK\r\n\r\n"
+                                    "SIP/2.0 200 OK\r\n\r\n" + SDP
 
+                                # Formado el SDP se contesta
                                 self.wfile.write(Answer)
+
+# =============================== ACK ====================================
                             elif METODO == "ACK":
+                                # LOG:
+                                Ack_log = "Received ACK from " + str(line[1][1]) \
+                                    + ":" + "200 OK [...]"
+                                log_status(Ack_log)
+
+                                # RTP
                                 print "Comienza la transmision........."
-                                Streaming = './mp32rtp -i 127.0.0.1 -p \
-                                    23032 <' + FILE
+                                Streaming = './mp32rtp -i ' + ip_destino \
+                                    + " -p " + port_rtp_dic[ip_destino]
+                                Streaming += " < " + myHandler.elementos["audio_path"]
                                 os.system(Streaming)
                                 print "Fin de la emision"
+
+                                # LOG:
+                                rtp_log = "Send RTP to " + str(line[1][1]) + \
+                                    ":" + "200 OK [...]"
+                                log_status(rtp_log)
+
+# =============================== BYE ===========================
                             elif METODO == "BYE":
+                                Bye_log = "Received BYE from " + \
+                                    + str(line[1]) + ":" + "200 OK [...]"
+                                log_status(Bye_log)
+
                                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
                     else:
                         self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
-#============================ PROGRAMA PRINCIPAL ===========================
+
+#============================ PROGRAMA PRINCIPAL ==================
 if __name__ == "__main__":
 # ======= OBTENCIÓN DE VALORES DEL FICHERO .xml =========
     try:
@@ -100,13 +153,16 @@ if __name__ == "__main__":
     myHandler = XMLHandler()
     parser.setContentHandler(myHandler)
     parser.parse(open(fich))
-    print myHandler.get_tags()
+    #print myHandler.get_tags()
 
     # Extraer campos del diccionario
     LOG_SERVER = myHandler.elementos["log_path"]
 
+    FILE = myHandler.elementos["audio_path"]
+
     UASERVER_IP = myHandler.elementos["uaserver_ip"]
     UASERVER_PORT = int(myHandler.elementos["uaserver_puerto"])
+    print "Puerto al que se ata", UASERVER_PORT
 
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", UASERVER_PORT), EchoHandler)
