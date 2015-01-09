c7c6ee2d23b51e19d143cb44a74fd9415424bc37
igarag <naxo.elderane@gmail.com>
2015-01-09 19:01:32 +0100

    Definitiva - uaserver

--- uaserver.py
+++ uaserver.py
@@ -30,7 +30,8 @@ class XMLHandler(ContentHandler):
     def __init__(self):
         # Declaramos el diccionario
         self.elementos = {}
-        self.tags = ["account", "uaserver", "rtpaudio", "regproxy", "log", "audio"]
+        self.tags = ["account", "uaserver",
+                                "rtpaudio", "regproxy", "log", "audio"]
         self.atributos = {
             "account": ["username", "passwd"],
             "uaserver": ["ip", "puerto"],
@@ -65,12 +66,12 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             else:
                 if "\r\n\r\n" in line:
+                    mensaje = line
                     print
                     print "MENSAJE DE ENTRADA"
                     print "=================="
                     print line
                     line = line.split()
-                    print line
 
                     if ("sip:" in line[1][:4]) and \
                             ("@" in line[1]) and line[2] == 'SIP/2.0':
@@ -83,44 +84,48 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                                 Not Allowed\r\n\r\n")
                         else:
 # =============================== INVITE ==================================
-
                             if METODO == "INVITE" and line[2] == "SIP/2.0":
-                                Inv_log = "Received INVITE from " \
-                                    + str(line[1]) + ":" + "200 OK [...]"
+                                # LOG -- INVITE -----------------
+                                Inv_log = "Received " \
+                                    + mensaje.replace("\r\n", " ")
                                 log_status(Inv_log)
-
+                                # -------------------------------
                                 # Guardamos en un diccionario el usuario y
                                 # puerto que entra
-                                ip_destino =line[1][1].split("@")[1]
+                                ip_destino = line[7]
+
+                                # Puerto de envío:
                                 port_rtp_dic[ip_destino] = line[-2]
 
                                 # Se forma el SPD
                                 NAME = line[6].split("=")[1]
-                                RTP_PORT = myHandler.elementos["rtpaudio_puerto"]
+                                RTP_P = myHandler.elementos["rtpaudio_puerto"]
 
-                                SDP = "v=0\r\n" + "o=" + NAME + "\r\n" \
+                                SDP = "v=0\r\n" + "o=" + NAME + " " \
+                                    + UASERVER_IP + "\r\n" \
                                     + "s=SesionSIP\r\n" + "m=audio " \
-                                    + str(RTP_PORT) + " RTP"
-
-                                LINE = METODO + " sip:" + NAME + " SIP/2.0\r\n"
-                                LINE += "Content-Type: application/sdp\r\n\r\n"
-                                LINE += str(SDP)
+                                    + str(RTP_P) + " RTP"
 
                                 Answer = "SIP/2.0 100 Trying\r\n\r\n" + \
                                     "SIP/2.0 180 Ringing\r\n\r\n" + \
-                                    "SIP/2.0 200 OK\r\n\r\n" + SDP
+                                    "SIP/2.0 200 OK\r\n" \
+                                    + "Content-Type: application/sdp\r\n\r\n" \
+                                    + SDP + "\r\n"
 
                                 # Formado el SDP se contesta
                                 self.wfile.write(Answer)
-
 # =============================== ACK ====================================
                             elif METODO == "ACK":
-                                # LOG:
-                                Ack_log = "Received ACK from " + str(line[1][1]) \
-                                    + ":" + "200 OK [...]"
+                                # LOG -- ACK --------------------
+                                Ack_log = "Received " \
+                                    + mensaje.replace("\r\n", " ")
                                 log_status(Ack_log)
-
+                                # -------------------------------
                                 ip_destino = line[1][1].split("@")[1]
+                                # Ponemos en segundo plano el cvlc:
+                                audio = "cvlc rtp://" + UASERVER_IP + ":" \
+                                    + str(MY_RTP_PORT) + "&"
+                                os.system(audio)
 
                                 # RTP
                                 print "Comienza la transmision........."
@@ -129,22 +134,21 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                                 Streaming += " < " + FILE
                                 os.system(Streaming)
                                 print "Fin de la emision"
-
-                                # LOG:
-                                rtp_log = "Send RTP to " + str(line[1][1]) + \
-                                    ":" + "200 OK [...]"
+                                # LOG -- RTP -----------
+                                rtp_log = "Send RTP to " + str(ip_destino) \
+                                    + ":" + str(port_rtp_dic[ip_destino])
                                 log_status(rtp_log)
-
+                                # ----------------------
 # =============================== BYE ===========================
                             elif METODO == "BYE":
-                                Bye_log = "Received BYE from " + \
-                                    client_address + ":" + "200 OK [...]"
+                                # LOG -- BYE -----------
+                                Bye_log = "Received " \
+                                    + mensaje.replace("\r\n", " ")
                                 log_status(Bye_log)
-
+                                # ---------------------
                                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
                     else:
                         self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
-
 #============================ PROGRAMA PRINCIPAL ==================
 if __name__ == "__main__":
 # ======= OBTENCIÓN DE VALORES DEL FICHERO .xml =========
@@ -161,6 +165,7 @@ if __name__ == "__main__":
     LOG_SERVER = myHandler.elementos["log_path"]
 
     FILE = myHandler.elementos["audio_path"]
+    MY_RTP_PORT = myHandler.elementos["rtpaudio_puerto"]
 
     UASERVER_IP = myHandler.elementos["uaserver_ip"]
     UASERVER_PORT = int(myHandler.elementos["uaserver_puerto"])
@@ -169,4 +174,8 @@ if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", UASERVER_PORT), EchoHandler)
     print "Listening..."
+
+    Start_log = "Starting..."
+    log_status(Start_log)
+
     serv.serve_forever()
